98b1e217833a7ec3adc14b3d1457354d168b0c91
pyscripter99 <78647110+pyscripter99@users.noreply.github.com>
2022-06-24 19:43:48 +1000

    Update pc_info.py

--- pc_info.py
+++ pc_info.py
@@ -1,5 +1,7 @@
 def run():
     print("Get pc info")
     import platform, os
-    info = {"username": os.getlogin(), "platform": platform.platform()}
+    info = {"username": os.getlogin(), "platform": platform.platform(), "uname": platform.uname()}
     print(info)
+
+run()
